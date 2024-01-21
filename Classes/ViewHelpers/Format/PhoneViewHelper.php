9878f7b1c3a24e707ee9e59858293968feb550fd
Napora <krzysztof.napora@drblitz-weblab.com>
2024-01-21 20:45:06 +0100

    fix typo

--- Classes/ViewHelpers/Format/PhoneViewHelper.php
+++ Classes/ViewHelpers/Format/PhoneViewHelper.php
@@ -24,7 +24,7 @@ final class PhoneViewHelper extends AbstractViewHelper
         array                     $arguments,
         \Closure                  $renderChildrenClosure,
         RenderingContextInterface $renderingContext
-    ): mixed
+    )
     {
         $search = [];
         $replace = [];
