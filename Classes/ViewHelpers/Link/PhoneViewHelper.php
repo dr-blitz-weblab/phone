3ecb0c2efe523ea35cfa3acae536e90ebf54f846
Krzysztof Napora <k.napora@drblitz-weblab.com>
2024-03-18 22:45:10 +0100

    Get region from site

--- Classes/ViewHelpers/Link/PhoneViewHelper.php
+++ Classes/ViewHelpers/Link/PhoneViewHelper.php
@@ -3,9 +3,10 @@
 namespace DrBlitz\Phone\ViewHelpers\Link;
 
 use libphonenumber\PhoneNumberUtil;
-use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
-use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
 
 final class PhoneViewHelper extends AbstractTagBasedViewHelper
 {
@@ -15,7 +16,7 @@ final class PhoneViewHelper extends AbstractTagBasedViewHelper
     {
         $this->registerUniversalTagAttributes();
         $this->registerArgument('phoneNumber', 'string', 'phone number', true);
-        $this->registerArgument('region', 'string', 'country', true);
+        $this->registerArgument('region', 'string', 'country', false);
         $this->registerArgument('format', 'int', 'The desired format', true);
     }
 
@@ -24,11 +25,33 @@ final class PhoneViewHelper extends AbstractTagBasedViewHelper
         if ($this->arguments['phoneNumber'] === null) {
             return (string)$this->renderChildren();
         }
+        if ($this->arguments['region'] === null) {
+            $context = GeneralUtility::makeInstance(Context::class);
+            /** @var \TYPO3\CMS\Core\Site\Entity\Site */
+            $site = $GLOBALS['TYPO3_REQUEST']->getAttribute('site');
+            $langId = $context->getPropertyFromAspect('language', 'id');
+            /** @var \TYPO3\CMS\Core\Site\Entity\SiteLanguage */
+            $language = $site->getLanguageById($langId);
+            $region = $language->getTwoLetterIsoCode();
+        } else {
+            $region = $this->arguments['region'];
+        }
+
         $phoneUtil = PhoneNumberUtil::getInstance();
-        $phoneNumber = $phoneUtil->parse($this->arguments['phoneNumber'], $this->arguments['region']);
+        $phoneNumber = $phoneUtil->parse($this->arguments['phoneNumber'], $region);
         $this->tag->addAttribute('href', $phoneUtil->format($phoneNumber, \libphonenumber\PhoneNumberFormat::RFC3966));
         $this->tag->setContent($phoneUtil->format($phoneNumber, (int)$this->arguments['format']));
         $this->tag->forceClosingTag(true);
         return $this->tag->render();
+    }
+
+    /**
+     * @return int
+     */
+    protected function getLanguageIdentifier(): int
+    {
+        /** @var SiteLanguage $language */
+        $language = $this->request->getAttribute('language');
+        return $language->getLanguageId();
     }
 }
